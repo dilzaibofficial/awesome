327549967d46f30e8fe220d8920d1f5ed7d2d51b
DIL ZAIB <dilzaib5848@gmail.com>
2022-10-20 01:34:04 +0500

    third

--- src/index.js
+++ src/index.js
@@ -1,17 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React, { createElement } from 'react'
+import ReactDOM from 'react-dom'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+ReactDOM.render(
+    <React.Fragment>
+        <h1> Thapa Technical </h1>
+        <p>abc</p>
+        <h1>DIL ZAIB CHANNEL</h1>
+    </React.Fragment>
+    ,
+    document.getElementById('root'))
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
+
+// const h1 = document.createElement('h1')
+// h1.innerHTML = "Hello World!"
+// document.getElementById('root').appendChild(h1)
