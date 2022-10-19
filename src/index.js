56956c7b99bde6d7ac86356015d5bdf82b3c6c86
DIL ZAIB <dilzaib5848@gmail.com>
2022-10-20 02:41:20 +0500

    EW

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import React, { createElement } from 'react'
 import ReactDOM from 'react-dom'
 
+const flname = "DILZAIB"
+
 ReactDOM.render(
     <React.Fragment>
-        <h1> Thapa Technical </h1>
+        <h1> my nam is {flname} </h1>
         <p>abc</p>
         <h1>DIL ZAIB CHANNEL</h1>
     </React.Fragment>
