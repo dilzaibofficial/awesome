987c4b4392821a63c05e7c7f73b2332be222ba07
DIL ZAIB <dilzaib5848@gmail.com>
2022-10-21 03:16:20 +0500

    1

--- src/index.js
+++ src/index.js
@@ -1,19 +1,21 @@
 import React, { createElement } from 'react'
 import ReactDOM from 'react-dom'
+import './index.css'
 
-const flname = "DILZAIB"
+
+const fname = "DIL"
+const lname = "ZAIB"
+const img2 = "https://source.unsplash.com/random/200x200?sig=1"
+const img1 = "https://source.unsplash.com/random/200x200?sig=2"
+const img3 = "https://source.unsplash.com/random/200x200?sig=3"
 
 ReactDOM.render(
     <React.Fragment>
-        <h1> my nam is {flname} </h1>
-        <p>abc</p>
-        <h1>DIL ZAIB CHANNEL</h1>
+        <h1 class="heading" >{`My Name Is ${fname} ${lname}`}</h1>
+        <p>My Lucky Number Is {5 + 5}</p>
+        <img src={img1} alt="Random Images" />
+        <img src={img2} alt="Random Images" />
+        <img src={img3} alt="Random Images" />
     </React.Fragment>
     ,
     document.getElementById('root'))
-
-
-
-// const h1 = document.createElement('h1')
-// h1.innerHTML = "Hello World!"
-// document.getElementById('root').appendChild(h1)
